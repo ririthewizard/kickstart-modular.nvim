9b52377746663c3d11922423bb5cda7a4297415a
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-24 21:31:45 -0500

    Working on it

--- lua/kickstart/plugins/nvim-dap.lua
+++ lua/kickstart/plugins/nvim-dap.lua
@@ -40,7 +40,7 @@ return {
           name = 'launch - netcoredbg',
           request = 'launch',
           program = function()
-            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+            return vim.fn.input('Path to dll ', vim.fn.getcwd() .. '/bin/Debug/net8.0', 'file')
           end,
         },
       }
