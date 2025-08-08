8d6a3a4c4c18f5d83ecf77db41cd5300e15175d2
ririthewizard <ririwilliams.rw@gmail.com>
2025-08-08 01:37:47 -0500

    Added a colorscheme and basic treesitter

---
+++ lua/config/colors/rosepine.lua
@@ -0,0 +1,11 @@
+vim.pack.add({
+	{ src ="https://github.com/rose-pine/neovim" }
+})
+
+require("rose-pine").setup({
+    styles = {
+	transparency = true,
+    },
+})
+
+vim.cmd[[colorscheme rose-pine]]
